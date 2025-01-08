495f1b5748f585cc6d0fce83880091f63d89ebc0
Maxence Caucheteux <caucheteux.maxence@gmail.com>
2025-01-08 11:07:44 +0100

    correction des erreurs

--- student+performance/students.py
+++ student+performance/students.py
@@ -36,5 +36,32 @@ print(f"Écart-type des échecs (sans Internet) : {s_wo:.2f}")
 # Test de Wald
 diff_means = m_wi-m_wo
 stat_observed = np.abs(diff_means)/np.sqrt(s_wi**2/n_wi+s_wo**2/n_wo)
-p_value = 1-norm.cdf(stat_observed)
+p_value = 2*(1-norm.cdf(stat_observed))
 print("p-valeur = ", p_value)
+
+# Pouvoir statistique
+alpha = 0.05
+phi = norm.ppf(1-alpha/2)
+r = np.sqrt(s_wi**2 / n_wi + s_wo**2 / n_wo)
+
+def power(x):
+    term1 = norm.cdf(phi-x/r)
+    term2 = norm.cdf(-phi-x/r)
+    return 1 - term1 + term2
+
+x = np.linspace(-1, 1, 1000)
+y = [power(xi) for xi in x]
+
+# Tracé
+plt.figure(figsize=(8, 6))
+plt.plot(x, y)
+plt.axhline(0, color='black', linewidth=0.8, linestyle='--')
+plt.axvline(0, color='black', linewidth=0.8, linestyle='--')
+plt.xlabel('$\Delta \mu = \mu_1 - \mu_2$', fontsize=14)
+plt.ylabel('Probability', fontsize=14)
+plt.title('Statistical power = 1 - type II error', fontsize=16)
+plt.grid(True)
+plt.show()
+
+
+
